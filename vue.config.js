7e46278e6f44cb98d2b63de5bb9cc9e2fd4ae33d
i.shunyaev <i.shunyaev@fact.digital>
2023-08-18 23:14:04 +0400

    [feat] added publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/zebra/'
+  : '/',
+
+  css: {
+    loaderOptions: {
+      scss: {
+        // eslint-disable-next-line quotes
+        additionalData: `@import '@/assets/styles/index.scss';`
+      }
+    }
+  }
 })
