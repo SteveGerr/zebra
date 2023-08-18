c6ca72f102efd5cf4a2c89927a6ea1d0a4feb8a8
i.shunyaev <i.shunyaev@fact.digital>
2023-08-18 23:08:08 +0400

    [feat] added news tag

---
+++ src/components/tag/AppTag.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tag"><slot></slot></div>
+</template>
+<script setup>
+</script>
+<style lang="scss" scoped>
+  .tag {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: .4rem 1.6rem;
+    @include text(1.4rem, 140%, 400);
+    color: $link-dark-default;
+    border-radius: $tag-border-radius;
+    background-color: $background-ultra-light-blue;
+  }
+</style>
