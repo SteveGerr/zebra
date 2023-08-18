b0de8c9e85850a884a7cef8c0e140311050e9ed0
i.shunyaev <i.shunyaev@fact.digital>
2023-08-18 23:07:28 +0400

    [feat] added header

---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="header">
+    <section class="header__logo">
+      <img class="header__logo-image" :src="logo" alt="">
+    </section>
+  </footer>
+</template>
+<script setup>
+import logo from '@/assets/svg/logo.svg'
+</script>
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  padding: 4rem 10rem;
+  background-color: $white;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+</style>
